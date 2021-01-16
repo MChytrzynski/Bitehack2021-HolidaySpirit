9558c2fcc29b8437d1523c8e03abd4020d65f7b1
Kamil Koziński <kamil.kozinski98@gmail.com>
2021-01-17 00:39:18 +0100

    problem view update

--- Front/HackFront/src/app/problem/problem.component.ts
+++ Front/HackFront/src/app/problem/problem.component.ts
@@ -11,7 +11,21 @@ export class ProblemComponent implements OnInit {
 
   problem:Problem;
   constructor(private route:ActivatedRoute) { }
-  
+  showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("img-slides") as HTMLCollectionOf < HTMLElement > ;
+    const dots = document.getElementsByClassName("images") as HTMLCollectionOf < HTMLElement > ;
+    slides[0].style.display = "block";
+   }
+  openModal() {
+    document.getElementById('imgModal').style.display = "block";
+   }
+  closeModal() {
+    document.getElementById('imgModal').style.display = "none";
+   }
+   currentSlide() {
+    this.showSlides();
+   }
 
   ngOnInit(): void {
     this.route.params.subscribe(x=>console.log(x.id))
